51397ee7c8056f0d0e74f74a9cfaedd5ba5057fb
David Coimbra <david.coimbra@tecnico.ulisboa.pt>
2018-02-26 19:52:39 +0000

    closes #14

--- tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/domain/TaxPayer.java
+++ tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/domain/TaxPayer.java
@@ -2,14 +2,13 @@ package pt.ulisboa.tecnico.softeng.tax.domain;
 
 
 public class TaxPayer {
-	private String _NIF;
+	private String _nif;
 	private String _name;
 	private String _address;
 	
-	public TaxPayer(String NIF,String NAME,String ADRESS) {
-		_NIF=NIF;
-		_name=NAME;
+	public TaxPayer(String NIF, String NAME, String ADDRESS) {
+		_nif = NIF;
+		_name = NAME;
 		_address = ADDRESS;
 	}
-
 }
