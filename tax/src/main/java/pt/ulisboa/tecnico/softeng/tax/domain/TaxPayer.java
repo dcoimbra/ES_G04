fa72a91cd83325362c6dc37f7257149a5f148574
Ivan Zarro <ivan.zarro@tecnico.ulisboa.pt>
2018-02-26 17:06:54 +0000

    closes #14

---
+++ tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/domain/TaxPayer.java
@@ -0,0 +1,15 @@
+package pt.ulisboa.tecnico.softeng.tax.domain;
+
+
+public class TaxPayer {
+	private String _NIF;
+	private String _name;
+	private String _address;
+	
+	public TaxPayer(NIF, NAME, ADRESS) {
+		_NIF=NIF;
+		_name=NAME;
+		_address = ADDRESS;
+	}
+
+}
