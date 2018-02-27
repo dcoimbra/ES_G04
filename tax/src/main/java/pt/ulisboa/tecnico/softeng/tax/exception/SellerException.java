cbe5dfc1d3866bf88c63cd5b7fd907b307cd724f
Joao Almeida <joao.santos.almeida@tecnico.ulisboa.pt>
2018-02-27 16:45:37 +0000

    closes #48

---
+++ tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/exception/SellerException.java
@@ -0,0 +1,11 @@
+package pt.ulisboa.tecnico.softeng.tax.exception;
+
+public class SellerException extends RuntimeException {
+  public SellerException() {
+    super();
+  }
+
+  public SellerException(String message) {
+    super(message);
+  }
+}
