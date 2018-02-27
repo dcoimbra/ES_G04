2e39233a974405ddaa879561957936086ae280e5
Ivan Zarro <ivan.zarro@tecnico.ulisboa.pt>
2018-02-27 14:24:07 +0000

    closes #21

---
+++ tax/src/test/java/pt/ulisboa/tecnico/softeng/tax/domain/ItemTypeConstructorTest.java
@@ -0,0 +1,44 @@
+package pt.ulisboa.tecnico.softeng.tax.domain;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ItemTypeConstructorTest {
+
+	private static final String ITEM_TYPE = "TOKEN";
+	private static final int TAX = "31";
+
+
+	@Test
+	public void success() {
+
+		ItemType itemtype = new ItemType(ITEM_TYPE, TAX);
+
+		Assert.assertEquals(ITEM_TYPE, itemtype.getITEM_TYPE());
+		Assert.assertEquals(TAX, itemtype.getTAX());
+	}
+
+	@Test(expected = ItemTypeException.class)
+	public void notStringITEM_TYPE() {
+		new ItemType(123, TAX);
+	}
+	
+	@Test
+	public void notUniqueITEM_TYPE() {
+		new ItemType(ITEM_TYPE, TAX);
+		try {
+			new ItemType(ITEM_TYPE, TAX);
+			Assert.fail();
+		} catch (ItemTypeException tpe) {
+			Assert.assertEquals(1, ItemType.itemtypes.size());
+		}
+	}
+
+	@Test(expected =  ItemTypeException.class)
+	public void notPositiveTAX() {
+		new TaxPayer(ITEM_TYPE, -20);
+	}
+
+}
