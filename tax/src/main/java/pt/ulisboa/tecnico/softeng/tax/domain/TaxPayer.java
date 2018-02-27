25cfd13b84e25b09615a83846ed2478e6fd5a172
Joao Almeida <joao.santos.almeida@tecnico.ulisboa.pt>
2018-02-27 14:17:03 +0000

    closes #20

--- tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/domain/TaxPayer.java
+++ tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/domain/TaxPayer.java
@@ -1,14 +1,38 @@
 package pt.ulisboa.tecnico.softeng.tax.domain;
 
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class TaxPayer {
+	
+	public static Set<TaxPayer> taxpayers = new HashSet<>();
+	
 	private String _nif;
 	private String _name;
 	private String _address;
 	
 	public TaxPayer(String NIF, String NAME, String ADDRESS) {
+		checkArguments(NIF, NAME, ADDRESS);
 		_nif = NIF;
 		_name = NAME;
 		_address = ADDRESS;
 	}
+	
+	private void checkArguments(String NIF, String NAME, String ADDRESS) {
+		if(NIF.length()!= 9) {
+			throw new TaxPayerException();
+		}
+		
+		for (TaxPayer tp : taxpayers) {
+			if (tp.getNIF().equals(NIF)) {
+				throw new TaxPayerException();
+			}
+		}
+	}
+	
+	public String getNIF() {
+		return this._nif;
+	}
 }
+
