4ede044bf9acc8250e91ecb809264606172ed24d
Samuel Santos <samuel.c.santos@tecnico.ulisboa.pt>
2018-03-01 18:44:09 +0000

    closes #8

---
+++ car/src/main/java/pt/ulisboa/tecnico/softeng/car/domain/Motorcycle.java
@@ -0,0 +1,8 @@
+package pt.ulisboa.tecnico.softeng.car.domain;
+
+public class Motorcycle extends Vehicle {
+	
+	public Motorcycle(String plate, int km, RentACar rentACar) {
+		super(plate, km, rentACar);
+	}
+}
