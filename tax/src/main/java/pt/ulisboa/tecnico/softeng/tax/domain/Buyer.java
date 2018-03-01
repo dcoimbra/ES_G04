bcb8f83c1be0a2ee7896f51aaebad57f5c79b2d7
David Coimbra <david.coimbra@tecnico.ulisboa.pt>
2018-03-01 21:07:39 +0000

    closes #43

--- tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/domain/Buyer.java
+++ tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/domain/Buyer.java
@@ -1,12 +1,9 @@
 package pt.ulisboa.tecnico.softeng.tax.domain;
 
-import java.util.HashSet;
-import java.util.Set;
-
-
 public class Buyer extends TaxPayer {
 	
 	public Buyer(String NIF, String NAME, String ADDRESS) {
+		
 		super(NIF, NAME, ADDRESS);
 	}
 }
--- tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/domain/Seller.java
+++ tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/domain/Seller.java
@@ -1,12 +1,9 @@
 package pt.ulisboa.tecnico.softeng.tax.domain;
 
-import java.util.HashSet;
-import java.util.Set;
-
-
 public class Seller extends TaxPayer {
 	
 	public Seller(String NIF, String NAME, String ADDRESS) {
+		
 		super(NIF, NAME, ADDRESS);
 	}
 }
