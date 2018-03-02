4ac5831ac6cc2609e545cac4973744f08af384f5
Ivan Zarro <ivan.zarro@tecnico.ulisboa.pt>
2018-03-02 17:27:36 +0000

    closes #28

--- tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/domain/Invoice.java
+++ tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/domain/Invoice.java
@@ -2,54 +2,60 @@ package pt.ulisboa.tecnico.softeng.tax.domain;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.time.LocalDate;
+import java.sql.Timestamp;
+
 
 
 public abstract class Invoice {
 	
-	public static Set<Invoice> Invoices = new HashSet<>();
+	public static Set<Invoice> invoices = new HashSet<>();
 	
-	private String _value ;
-	private String _date ;
+	private float _value ;
+	private LocalDate _date ;
 	private String _itemType ;
-	private String _seller ;
-	private String _buyer ;
+	private Seller _seller ;
+	private Buyer _buyer ;
 	private float _iva;
+	private String _reference;
 	
-	public Invoice(String  VALUE, String DATE, String ITEM_TYPE,String SELLER,String BUYER) {
-		checkArguments(VALUE, DATE, ITEM_TYPE,SELLER,BUYER);
+	
+	public Invoice(float value, LocalDate date, String item_type,Seller seller, Buyer buyer) {
+		checkArguments(value, date, item_type,seller,buyer);
 
-		_iva = ItemType.itemtypes.get(ITEM_TYPE)/100;
-		_value =VALUE;
-		_date = DATE;
-		_itemType = ITEM_TYPE;
-		_seller = SELLER;
-		_buyer = BUYER ;
-		Invoices.add(this);
+		_iva = ItemType.itemtypes.get(item_type)/100;
+		_value =value;
+		_date = date;
+		_itemType = item_type;
+		_seller = seller;
+		_buyer = buyer ;
+		_reference= new Timestamp(0).toString();
+		invoices.add(this);
 	}
 	
-	private void checkArguments(String  VALUE, String DATE, String ITEM_TYPE,String SELLER,String BUYER) {
+	private void checkArguments(float VALUE, LocalDate DATE, String ITEM_TYPE,Seller SELLER,Buyer BUYER) {
 		
-		if(VALUE=="" ||VALUE==null || DATE=="" || DATE==null || ITEM_TYPE=="" || ITEM_TYPE==null || SELLER=="" || SELLER==null || BUYER=="" || BUYER==null) {
+		if(DATE==null || ITEM_TYPE=="" || ITEM_TYPE==null || SELLER==null || BUYER==null) {
 			throw new InvoiceException();
 		}
 		
-		if(Integer.parseInt(DATE) < 1970) {
+		if(DATE.getYear() < 1970) {
 			throw new InvoiceException();
 		}
 			
 		
 		for (Invoice tp : Invoices) {
-			if (tp.getVALUE().equals(VALUE) || tp.getDATE().equals(DATE)  || tp.getITEM_TYPE().equals(ITEM_TYPE) || tp.getSELLER().equals(SELLER)  || tp.getBUYER().equals(BUYER) ){
+			if (tp.getVALUE()==(VALUE) || tp.getDATE().equals(DATE)  || tp.getITEM_TYPE().equals(ITEM_TYPE) || tp.getSELLER().equals(SELLER)  || tp.getBUYER().equals(BUYER) ){
 				throw new InvoiceException();
 			}
 		}
 	}
 	
-	public String getVALUE() {
+	public float getVALUE() {
 		return this._value;
 	}
 	
-	public String getDATE() {
+	public LocalDate getDATE() {
 		return this._date;
 	}
 	
@@ -57,15 +63,15 @@ public abstract class Invoice {
 		return this._itemType;
 	}
 	
-	public String getSELLER() {
+	public Seller getSELLER() {
 		return this._seller;
 	}
 	
-	public String getBUYER() {
+	public Buyer getBUYER() {
 		return this._buyer;
 	}
 
-	public int getIva(){
+	public float getIva(){
 		return this._iva;
 	}
 }
--- tax/src/test/java/pt/ulisboa/tecnico/softeng/tax/domain/InvoiceConstructorTest.java
+++ tax/src/test/java/pt/ulisboa/tecnico/softeng/tax/domain/InvoiceConstructorTest.java
@@ -1,5 +1,8 @@
 package pt.ulisboa.tecnico.softeng.tax.domain;
 
+import java.time.LocalDate;
+import java.time.Local_date;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -9,96 +12,98 @@ import pt.ulisboa.tecnico.softeng.tax.exception.InvoiceException;
 
 public class InvoiceConstructorTest {
     
-	private static final String VALUE = "2";
-	private static final String DATE = "13/3/2000";
-	private static final String ITEM_TYPE = "TOKEN";
-	private static final String SELLER = "tu";
-	private static final String BUYER = "eu";
+
+	private static final float _value =1.5f;
+	private static final LocalDate _date ;
+	private static final String _itemType ="token" ;
+	private static final Seller _seller ;
+	private static final Buyer _buyer ;
+	private static final float _iva;
 
 
 	@Test
 	public void success() {
 
-		Invoice Invoice = new Invoice(VALUE, DATE, ITEM_TYPE, SELLER, BUYER);
+		Invoice Invoice = new Invoice(_value, _date, _itemType, _seller, _buyer);
 
-		Assert.assertEquals(VALUE, Invoice.getVALUE());
-		Assert.assertEquals(DATE, Invoice.getDATE());
-		Assert.assertEquals(ITEM_TYPE, Invoice.getITEM_TYPE());
-		Assert.assertEquals(SELLER, Invoice.getSELLER());
-		Assert.assertEquals(BUYER, Invoice.getBUYER());
+		Assert.assertEquals(_value, Invoice.get_value());
+		Assert.assertEquals(_date, Invoice.get_date());
+		Assert.assertEquals(_itemType, Invoice.get_itemType());
+		Assert.assertEquals(_seller, Invoice.get_seller());
+		Assert.assertEquals(_buyer, Invoice.get_buyer());
 	}
 
 	@Test(expected = InvoiceException.class)
-	public void notStringVALUE() {
-		new Invoice(null, DATE, ITEM_TYPE, SELLER, BUYER);
+	public void notString_value() {
+		new Invoice(null, _date, _itemType, _seller, _buyer);
 	}
 
 	@Test
-	public void ivaTaxValue(){
-		ItemType it = new ItemType (ITEM_TYPE, 20);
-		Invoice invoice = new Invoice(VALUE, DATE, ITEM_TYPE, SELLER, BUYER);
+	public void ivaTax_value(){
+		ItemType it = new ItemType (_itemType, 20);
+		Invoice invoice = new Invoice(_value, _date, _itemType, _seller, _buyer);
 
-		int tax = ItemType.itemtypes.get(ITEM_TYPE);
+		int tax = ItemType.itemtypes.get(_itemType);
 
 		Assert.assertEquals(tax/100, Invoice.getIva())
 	}
 	
 	@Test(expected = InvoiceException.class)
-	public void notEmptyVALUE() {
-		new Invoice("", DATE, ITEM_TYPE, SELLER, BUYER);
+	public void notEmpty_value() {
+		new Invoice("", _date, _itemType, _seller, _buyer);
 	}
 	
 	@Test(expected = InvoiceException.class)
 	public void notStringDATA() {
-		new Invoice(VALUE, null, ITEM_TYPE , SELLER, BUYER);
+		new Invoice(_value, null, _itemType , _seller, _buyer);
 	}
 	
 	@Test(expected = InvoiceException.class)
 	public void notEmptyDATA() {
-		new Invoice(VALUE, "", ITEM_TYPE , SELLER, BUYER);
+		new Invoice(_value, "", _itemType , _seller, _buyer);
 	}
 	
 	@Test(expected = InvoiceException.class)
 	public void notValidDATA() {
-		new Invoice(VALUE, "13/3/1969", ITEM_TYPE , SELLER, BUYER);
+		new Invoice(_value, "13/3/1969", _itemType , _seller, _buyer);
 	}
 	
 	@Test(expected = InvoiceException.class)
-	public void notStringITEM_TYPE() {
-		new Invoice(VALUE, DATE, null, SELLER, BUYER);
+	public void notString_itemType() {
+		new Invoice(_value, _date, null, _seller, _buyer);
 	}
 	
 	@Test(expected = InvoiceException.class)
-	public void notEmptyITEM_TYPE() {
-		new Invoice(VALUE, DATE, "", SELLER, BUYER);
+	public void notEmpty_itemType() {
+		new Invoice(_value, _date, "", _seller, _buyer);
 	}
 	
 	@Test(expected = InvoiceException.class)
-	public void notStringSELLER() {
-		new Invoice(VALUE, DATE, ITEM_TYPE, null, BUYER);
+	public void notString_seller() {
+		new Invoice(_value, _date, _itemType, null, _buyer);
 	}
 	
 	@Test(expected = InvoiceException.class)
-	public void notEmptySELLER() {
-		new Invoice(VALUE, DATE, ITEM_TYPE, "", BUYER);
+	public void notEmpty_seller() {
+		new Invoice(_value, _date, _itemType, "", _buyer);
 	}
 	
 	@Test(expected = InvoiceException.class)
-	public void notStringBUYER() {
-		new Invoice(VALUE, DATE, ITEM_TYPE, SELLER, null);
+	public void notString_buyer() {
+		new Invoice(_value, _date, _itemType, _seller, null);
 	}
 	
 	@Test(expected = InvoiceException.class)
-	public void notEmptyBUYER() {
-		new Invoice(VALUE, DATE, ITEM_TYPE, SELLER, "");
+	public void notEmpty_buyer() {
+		new Invoice(_value, _date, _itemType, _seller, "");
 	}
 	
 
 	@Test
 	public void notUnique() {
-		new Invoice(VALUE, DATE, ITEM_TYPE, SELLER, BUYER);
+		new Invoice(_value, _date, _itemType, _seller, _buyer);
 		try {
-			new Invoice(VALUE, DATE, ITEM_TYPE, SELLER, BUYER);
+			new Invoice(_value, _date, _itemType, _seller, _buyer);
 			Assert.fail();
 		} catch (InvoiceException tpe) {
 			Assert.assertEquals(1, Invoice.Invoices.size());
