38ebbfe69b066aa8ec170e0517c2e401335834ed
Ivan Zarro <ivan.zarro@tecnico.ulisboa.pt>
2018-03-08 22:51:06 +0000

    closes #105

---
+++ tax/src/test/java/pt/ulisboa/tecnico/softeng/tax/domain/IRSgetItemTypeByNameMethodTest.java
@@ -0,0 +1,38 @@
+package pt.ulisboa.tecnico.softeng.tax.domain;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import pt.ulisboa.tecnico.softeng.tax.exception.IRSException;
+import pt.ulisboa.tecnico.softeng.tax.exception.ItemTypeException;
+
+public class IRSgetItemTypeByNameMethodTest {
+    
+	private ItemType it1 = new ItemType("token1", 20);
+
+	@Before
+	public void setUp() {
+		IRS._itemtypes.clear();
+		ItemType._itemtypes.clear();
+		IRS._itemtypes.add(it1);
+		
+	}
+
+	@Test(expected = ItemTypeException.class)
+	public void noItemType(){
+		IRS.getIRS().getItemTypeByName("token2");
+	}
+
+	@Test
+	public void existsItemType(){
+		IRS.getIRS().getItemTypeByName("token1");
+	}
+
+	@After 
+	public void tearDown() {
+		IRS._itemtypes.clear();
+		ItemType._itemtypes.clear();
+	}
+}
