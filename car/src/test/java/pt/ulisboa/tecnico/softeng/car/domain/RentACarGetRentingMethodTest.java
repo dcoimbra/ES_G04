bdb96a41b46f0dfcb0f2894641dca177b53dae3b
Bernardo Andrade <bernardo.andrade@tecnico.ulisboa.pt>
2018-03-08 23:55:57 +0000

    closes #138

--- car/src/test/java/pt/ulisboa/tecnico/softeng/car/domain/RentACarGetRentingMethodTest.java
+++ car/src/test/java/pt/ulisboa/tecnico/softeng/car/domain/RentACarGetRentingMethodTest.java
@@ -8,12 +8,25 @@ import org.joda.time.LocalDate;
 import pt.ulisboa.tecnico.softeng.car.exception.CarException;
 
 public class RentACarGetRentingMethodTest {
-	private static final String DRIVING_LICENSE = "VC12345";
-	private static final String PLATE = "XX-XX-XX";
-	private static final int KM = 100;
-
+	private static final int VEHICLE_KM = 10;
+	private static final String VEHICLE_PLATE = "XX-XX-XX";
+	private static final String DRIVING_LICENSE = "VC1";
+	
+	
 	private RentACar _rentCar = new RentACar("Benecar");
 
+	@Test
+	public void success() {	
+		Motorcycle m1 = new Motorcycle(VEHICLE_PLATE, VEHICLE_KM, this._rentCar);
+		m1.rent(DRIVING_LICENSE, new LocalDate(2014,1,4) ,  new LocalDate(2014, 1, 5));
+		m1.rent(DRIVING_LICENSE, new LocalDate(2014,1,10) ,  new LocalDate(2014, 1, 11));
+		m1.rent(DRIVING_LICENSE, new LocalDate(2014,1,15) ,  new LocalDate(2014, 1, 16));
+		m1.rent(DRIVING_LICENSE, new LocalDate(2014,1,20) ,  new LocalDate(2014, 1, 21));
+
+		Assert.assertEquals(m1.getRentings().iterator().next().getReference(), _rentCar.getRenting(m1.getRentings().iterator().next().getReference()).getReference());
+	}
+	
+	
 	@Test(expected = CarException.class)
 	public void nullReference() {
 		this._rentCar.getRenting(null);
