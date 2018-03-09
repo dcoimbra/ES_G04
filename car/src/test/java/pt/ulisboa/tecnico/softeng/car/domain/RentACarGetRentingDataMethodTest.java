a61caa525d96eff3e18ceaebf50201c13f2250c6
Bernardo Andrade <bernardo.andrade@tecnico.ulisboa.pt>
2018-03-09 23:43:30 +0000

    closes #142

--- car/src/test/java/pt/ulisboa/tecnico/softeng/car/domain/RentACarGetRentingDataMethodTest.java
+++ car/src/test/java/pt/ulisboa/tecnico/softeng/car/domain/RentACarGetRentingDataMethodTest.java
@@ -1,31 +1,35 @@
 package pt.ulisboa.tecnico.softeng.car.domain;
 
-import static org.junit.Assert.assertEquals;
-
 import org.joda.time.LocalDate;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.After;
 
-import pt.ulisboa.tecnico.softeng.car.dataobjects.RentingData;
 import pt.ulisboa.tecnico.softeng.car.exception.CarException;
 
 public class RentACarGetRentingDataMethodTest {
 	private static final String DRIVINGLICENSE = "VN12345";
-	private static final LocalDate BEGIN = new LocalDate(2018,5,2);
-	private static final LocalDate END = new LocalDate(2018,5,12);
-	private static final String RENTACARCODE = "1";
-	private static final String REFERENCE = "0012";
-	private static final String PLATE = "XX-XX-XX";
+	private static final String PLATE1 = "XX-XX-XX";
+	private static final String PLATE2 = "XX-XX-XX";
 	private static final int KM = 1;
 
 	@Test
 	public void success() {
 		RentACar rentCar = new RentACar("Benecar");
-		Vehicle m1 = new Motorcycle(PLATE, KM, rentCar);
+		RentACar rentCar2 = new RentACar("AutoLDA");
+		Vehicle m1 = new Motorcycle(PLATE1, KM, rentCar);
+		Vehicle m2 = new Motorcycle(PLATE2, KM, rentCar2);
 		m1.rent(DRIVINGLICENSE, new LocalDate(2014,1,1) ,  new LocalDate(2014, 1, 3));
+		m1.rent(DRIVINGLICENSE, new LocalDate(2014,1,4) ,  new LocalDate(2014, 1, 6));
+		m1.rent(DRIVINGLICENSE, new LocalDate(2014,1,8) ,  new LocalDate(2014, 1, 10));
+		m2.rent(DRIVINGLICENSE, new LocalDate(2014,2,1) ,  new LocalDate(2014, 2, 3));
+		m2.rent(DRIVINGLICENSE, new LocalDate(2014,2,4) ,  new LocalDate(2014, 2, 6));
+		m2.rent(DRIVINGLICENSE, new LocalDate(2014,2,8) ,  new LocalDate(2014, 2, 10));
+		String reference = null;
 		
 		for(Renting r : m1.getRentings())
-			Assert.assertNotNull(RentACar.getRentingData(r.getReference()));
+			reference = r.getReference();
+		Assert.assertNotNull(RentACar.getRentingData(reference));
 	}
 	
 	@Test(expected = CarException.class)
@@ -46,6 +50,11 @@ public class RentACarGetRentingDataMethodTest {
 	@Test(expected = CarException.class)
 	public void referenceNotExists() {
 		RentACar.getRentingData("XPTO");
+	}
+
+	@After
+	public void tearDown() {
+		RentACar.rents.clear();
 	}
 
 }
--- car/src/test/java/pt/ulisboa/tecnico/softeng/car/domain/RentACarGetRentingMethodTest.java
+++ car/src/test/java/pt/ulisboa/tecnico/softeng/car/domain/RentACarGetRentingMethodTest.java
@@ -3,13 +3,15 @@ package pt.ulisboa.tecnico.softeng.car.domain;
 import org.junit.Assert;
 import org.junit.Test;
 import org.joda.time.LocalDate;
+import org.junit.After;
 
 
 import pt.ulisboa.tecnico.softeng.car.exception.CarException;
 
 public class RentACarGetRentingMethodTest {
 	private static final int VEHICLE_KM = 10;
-	private static final String VEHICLE_PLATE = "XX-XX-XX";
+	private static final String VEHICLE_PLATE1 = "XX-XX-XX";
+	private static final String VEHICLE_PLATE2 = "YY-YY-YY";
 	private static final String DRIVING_LICENSE = "VC1";
 	
 	
@@ -17,13 +19,18 @@ public class RentACarGetRentingMethodTest {
 
 	@Test
 	public void success() {	
-		Motorcycle m1 = new Motorcycle(VEHICLE_PLATE, VEHICLE_KM, this._rentCar);
+		Motorcycle m1 = new Motorcycle(VEHICLE_PLATE1, VEHICLE_KM, this._rentCar);
+		Motorcycle m2 = new Motorcycle(VEHICLE_PLATE2, VEHICLE_KM, this._rentCar);
 		m1.rent(DRIVING_LICENSE, new LocalDate(2014,1,4) ,  new LocalDate(2014, 1, 5));
 		m1.rent(DRIVING_LICENSE, new LocalDate(2014,1,10) ,  new LocalDate(2014, 1, 11));
 		m1.rent(DRIVING_LICENSE, new LocalDate(2014,1,15) ,  new LocalDate(2014, 1, 16));
 		m1.rent(DRIVING_LICENSE, new LocalDate(2014,1,20) ,  new LocalDate(2014, 1, 21));
+		m2.rent(DRIVING_LICENSE, new LocalDate(2014,2,4) ,  new LocalDate(2014, 2, 5));
+		m2.rent(DRIVING_LICENSE, new LocalDate(2014,2,10) ,  new LocalDate(2014, 2, 11));
+		m2.rent(DRIVING_LICENSE, new LocalDate(2014,2,15) ,  new LocalDate(2014, 2, 16));
+		m2.rent(DRIVING_LICENSE, new LocalDate(2014,2,20) ,  new LocalDate(2014, 2, 21));
 
-		Assert.assertEquals(m1.getRentings().iterator().next().getReference(), _rentCar.getRenting(m1.getRentings().iterator().next().getReference()).getReference());
+		Assert.assertEquals(m2.getRentings().iterator().next().getReference(), _rentCar.getRenting(m2.getRentings().iterator().next().getReference()).getReference());
 	}
 	
 	
@@ -45,6 +52,11 @@ public class RentACarGetRentingMethodTest {
 	@Test(expected = CarException.class)
 	public void emptySetOfReferences() {
 		this._rentCar.getRenting("XPTO");
+	}
+
+	@After
+	public void tearDown() {
+		RentACar.rents.clear();
 	}
 
 }
