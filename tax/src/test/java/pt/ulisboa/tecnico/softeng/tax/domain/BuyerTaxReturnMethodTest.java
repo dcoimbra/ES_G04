511ff26146e9afb77b0a8c7404a657be55f7b4d3
Joao Almeida <joao.santos.almeida@tecnico.ulisboa.pt>
2018-03-13 19:29:58 +0000

    closes #149

--- tax/src/test/java/pt/ulisboa/tecnico/softeng/tax/domain/BuyerTaxReturnMethodTest.java
+++ tax/src/test/java/pt/ulisboa/tecnico/softeng/tax/domain/BuyerTaxReturnMethodTest.java
@@ -1,24 +1,29 @@
 package pt.ulisboa.tecnico.softeng.tax.domain;
 
+import org.joda.time.LocalDate;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
-import org.joda.time.LocalDate;
 
 import pt.ulisboa.tecnico.softeng.tax.exception.TaxPayerException;
 
 public class BuyerTaxReturnMethodTest {
 
 	private Buyer buyer;
+    private Invoice i;
+    private ItemType it;
 
 	@Before
 	public void setUp() {
 		IRS.getIRS()._taxpayers.clear();
+		IRS.getIRS()._itemtypes.clear();
+		Invoice._invoices.clear();
+		
 		this.buyer = new Buyer("123456789", "Antonio", "Quinta das Lagrimas");
+		ItemType it = new ItemType("token1", 29);
+		this.i = new Invoice(1.5f, new LocalDate().now(), "token1" ,new Seller("123456780", "Jose Toni", "Quinta das Lagrimas") , this.buyer);
 	}
-
 
 	@Test(expected = TaxPayerException.class)
 	public void beforeYear() {
@@ -31,12 +36,20 @@ public class BuyerTaxReturnMethodTest {
 	}
 
 	@Test
+	public void thisYear() {
+		this.buyer.taxReturn(1971);
+	}
+	
+	@Test
 	public void afterYear() {
-		this.buyer.taxReturn(1972);
+		this.buyer.taxReturn(2018);
 	}
 
 	@After 
 	public void tearDown() {
 		IRS.getIRS()._taxpayers.clear();
+		IRS.getIRS()._itemtypes.clear();
+		Invoice._invoices.clear();
 	}
 }
+
