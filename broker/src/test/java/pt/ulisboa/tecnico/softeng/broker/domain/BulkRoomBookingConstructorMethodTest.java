00fb36b6ab94c3fdd1420dabac477ed1d55066ea
José Ferrão <jozeus.alves@gmail.com>
2018-03-14 22:38:54 +0000

    closes #144

--- broker/src/test/java/pt/ulisboa/tecnico/softeng/broker/domain/BulkRoomBookingConstructorMethodTest.java
+++ broker/src/test/java/pt/ulisboa/tecnico/softeng/broker/domain/BulkRoomBookingConstructorMethodTest.java
@@ -1,7 +1,7 @@
 package pt.ulisboa.tecnico.softeng.broker.domain;
 
-import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import org.joda.time.LocalDate;
@@ -10,52 +10,75 @@ import pt.ulisboa.tecnico.softeng.broker.exception.BrokerException;
 
 public class BulkRoomBookingConstructorMethodTest {
 
+	private BulkRoomBooking _bulkRoomBooking;
+
 	private static final int BOOKING_NUMBER = 10;
 	private static final LocalDate  BOOKING_ARRIVAL = new LocalDate(2017, 02, 02);
 	private static final LocalDate BOOKING_DEPARTURE = new LocalDate(2017, 02, 10);
 
+	@Before
+	public void setUp () {
+		this._bulkRoomBooking = new BulkRoomBooking(BOOKING_NUMBER, BOOKING_ARRIVAL, BOOKING_DEPARTURE);
+	}
+
 	@Test
 	public void success () {
-		BulkRoomBooking bulkRoomBooking = new BulkRoomBooking(BOOKING_NUMBER, BOOKING_ARRIVAL, BOOKING_DEPARTURE);
-
-		Assert.assertEquals(BOOKING_NUMBER, bulkRoomBooking.getNumber());
-		Assert.assertEquals(BOOKING_ARRIVAL, bulkRoomBooking.getArrival());
-		Assert.assertEquals(BOOKING_DEPARTURE, bulkRoomBooking.getDeparture());
+		Assert.assertEquals(BOOKING_NUMBER, this._bulkRoomBooking.getNumber());
+		Assert.assertEquals(BOOKING_ARRIVAL, this._bulkRoomBooking.getArrival());
+		Assert.assertEquals(BOOKING_DEPARTURE, this._bulkRoomBooking.getDeparture());
 	}
 
-	@Test(expected = BrokerException.class)
+	@Test
 	public void zeroNumber () {
-		new BulkRoomBooking(0, BOOKING_ARRIVAL, BOOKING_DEPARTURE);
+		try {
+			new BulkRoomBooking(0, BOOKING_ARRIVAL, BOOKING_DEPARTURE);
+		} catch (BrokerException e) {
+			Assert.fail();
+		}
 	}
 
-	@Test(expected = BrokerException.class)
+	@Test
 	public void negativeNumber () {
-		new BulkRoomBooking(-BOOKING_NUMBER, BOOKING_ARRIVAL, BOOKING_DEPARTURE);
+		try {
+			new BulkRoomBooking(-BOOKING_NUMBER, BOOKING_ARRIVAL, BOOKING_DEPARTURE);
+		} catch (BrokerException e) {
+			Assert.fail();
+		}
 	}
 
-	@Test(expected = BrokerException.class)
+	@Test
 	public void nullArrivalDate () {
-		new BulkRoomBooking(BOOKING_NUMBER, null, BOOKING_DEPARTURE);
+		try {
+			new BulkRoomBooking(BOOKING_NUMBER, null, BOOKING_DEPARTURE);
+		} catch (BrokerException e) {
+			Assert.fail();
+		}
 	}
 
-	@Test(expected = BrokerException.class)
+	@Test
 	public void nullDepartureDate () {
-		new BulkRoomBooking(BOOKING_NUMBER, BOOKING_ARRIVAL, null);
+		try {
+			new BulkRoomBooking(BOOKING_NUMBER, BOOKING_ARRIVAL, null);
+		} catch (BrokerException e) {
+			Assert.fail();
+		}
 	}
 
-	@Test(expected = BrokerException.class)
+	@Test
 	public void nullBothDates () {
-		new BulkRoomBooking(BOOKING_NUMBER, null, null);
+		try {
+			new BulkRoomBooking(BOOKING_NUMBER, null, null);
+		} catch (BrokerException e) {
+			Assert.fail();
+		}
 	}
 
-	//Tests if Arrival Date is before than Departure Date
-	@Test(expected = BrokerException.class)
+	@Test
 	public void invalidDates () {
-		new BulkRoomBooking(BOOKING_NUMBER, BOOKING_DEPARTURE, BOOKING_ARRIVAL);
-	}
-
-	@After
-	public void tearDown() {
-
+		try {
+			new BulkRoomBooking(BOOKING_NUMBER, BOOKING_DEPARTURE, BOOKING_ARRIVAL);
+		} catch (BrokerException e) {
+			Assert.fail();
+		}
 	}
 }
