e45aa068346b68b64dc50257806581bfcf792455
Ivan Zarro <ivan.zarro@tecnico.ulisboa.pt>
2018-03-28 18:38:58 +0100

    closes #203

---
+++ broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/RentVehicleState.java
@@ -0,0 +1,36 @@
+package pt.ulisboa.tecnico.softeng.broker.domain;
+
+import pt.ulisboa.tecnico.softeng.broker.domain.Adventure.State;
+import pt.ulisboa.tecnico.softeng.broker.exception.RemoteAccessException;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.CarInterface;
+import pt.ulisboa.tecnico.softeng.car.exception.CarException;
+
+public class RentVehicleState extends AdventureState {
+	public static final int MAX_REMOTE_ERRORS = 5;
+	
+	@Override
+	public State getState() {
+		return State.RENT_VEHICLE;
+	}
+
+	@Override
+	public void process(Adventure adventure) {
+		try {
+			adventure.setVehicleConfirmation(
+					CarInterface.rentVehicle("plate","license", adventure.getBegin(), adventure.getEnd()));
+		} catch (CarException ce) {
+			incNumOfRemoteErrors();
+			adventure.setState(State.UNDO);
+			return;
+		} catch (RemoteAccessException rae) {
+			incNumOfRemoteErrors();
+			if (getNumOfRemoteErrors() == MAX_REMOTE_ERRORS) {
+				adventure.setState(State.UNDO);
+			}
+			return;
+		}
+
+		adventure.setState(State.CONFIRMED);
+	}
+
+}
