af31df6176faad120e7966ecd1509203b12d082a
Samuel Santos <samuel.c.santos@tecnico.ulisboa.pt>
2018-04-02 17:58:13 +0100

    closes #165

--- hotel/src/main/java/pt/ulisboa/tecnico/softeng/hotel/domain/Hotel.java
+++ hotel/src/main/java/pt/ulisboa/tecnico/softeng/hotel/domain/Hotel.java
@@ -15,18 +15,23 @@ public class Hotel {
 
 	private final String code;
 	private final String name;
+	private final String nif;
+	private final String iban;
 	private final Set<Room> rooms = new HashSet<>();
 
-	public Hotel(String code, String name) {
-		checkArguments(code, name);
+	public Hotel(String code, String name, String nif, String iban) {
+		checkArguments(code, name, nif, iban);
 
 		this.code = code;
 		this.name = name;
+		this.nif = nif;
+		this.iban = iban;
 		Hotel.hotels.add(this);
 	}
 
-	private void checkArguments(String code, String name) {
-		if (code == null || name == null || code.trim().length() == 0 || name.trim().length() == 0) {
+	private void checkArguments(String code, String name, String nif, String iban) {
+		if (code == null || name == null || code.trim().length() == 0 || name.trim().length() == 0 ||
+			nif == null	|| nif.trim().length() == 0 || iban == null || iban.trim().length() == 0) {
 			throw new HotelException();
 		}
 
@@ -61,7 +66,15 @@ public class Hotel {
 	public String getName() {
 		return this.name;
 	}
+	
+	public String getNif() {
+		return this.nif;
+	}
 
+	public String getIban() {
+		return this.iban;
+	}
+	
 	void addRoom(Room room) {
 		if (hasRoom(room.getNumber())) {
 			throw new HotelException();
