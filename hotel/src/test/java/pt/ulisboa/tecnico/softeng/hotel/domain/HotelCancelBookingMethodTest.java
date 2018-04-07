911ddef0fe11bb647fb04acecde9c116fff257ce
Samuel Santos <samuel.c.santos@tecnico.ulisboa.pt>
2018-04-07 23:58:49 +0100

    closes #214

--- hotel/src/test/java/pt/ulisboa/tecnico/softeng/hotel/domain/HotelCancelBookingMethodTest.java
+++ hotel/src/test/java/pt/ulisboa/tecnico/softeng/hotel/domain/HotelCancelBookingMethodTest.java
@@ -1,15 +1,27 @@
 package pt.ulisboa.tecnico.softeng.hotel.domain;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.joda.time.LocalDate;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import mockit.Expectations;
+import mockit.Mocked;
 import pt.ulisboa.tecnico.softeng.hotel.domain.Room.Type;
 import pt.ulisboa.tecnico.softeng.hotel.exception.HotelException;
+import pt.ulisboa.tecnico.softeng.hotel.interfaces.BankInterface;
+import pt.ulisboa.tecnico.softeng.hotel.interfaces.TaxInterface;
+import pt.ulisboa.tecnico.softeng.tax.dataobjects.InvoiceData;
 
 public class HotelCancelBookingMethodTest {
+	private static final String IBAN = "IBAN";
+	private static final String NIF = "123456789";
+	private static final double PRICE_SINGLE = 20.0;
+	private static final double PRICE_DOUBLE = 30.0;
 	private final LocalDate arrival = new LocalDate(2016, 12, 19);
 	private final LocalDate departure = new LocalDate(2016, 12, 21);
 	private Hotel hotel;
@@ -18,9 +30,43 @@ public class HotelCancelBookingMethodTest {
 
 	@Before
 	public void setUp() {
-		this.hotel = new Hotel("XPTO123", "Paris");
+		this.hotel = new Hotel("XPTO123", "Paris", "NIF", IBAN, PRICE_SINGLE, PRICE_DOUBLE);
 		this.room = new Room(this.hotel, "01", Type.DOUBLE);
 		this.booking = this.room.reserve(Type.DOUBLE, this.arrival, this.departure);
+	}
+
+	@Test
+	public void success(@Mocked final TaxInterface taxInterface, @Mocked final BankInterface bankInterface) {
+		new Expectations() {
+			{
+				BankInterface.processPayment(this.anyString, this.anyDouble);
+
+				TaxInterface.submitInvoice((InvoiceData) this.any);
+			}
+		};
+
+		Booking booking = new Booking(Type.DOUBLE, this.hotel, this.arrival, this.departure, NIF);
+		this.hotel.getProcessor().submitBooking(booking);
+
+		String cancel = Hotel.cancelBooking(booking.getReference());
+
+		assertTrue(booking.isCancelled());
+		assertEquals(cancel, booking.getCancellation());
+	}
+
+	@Test(expected = HotelException.class)
+	public void doesNotExist(@Mocked final TaxInterface taxInterface, @Mocked final BankInterface bankInterface) {
+		new Expectations() {
+			{
+				BankInterface.processPayment(this.anyString, this.anyDouble);
+
+				TaxInterface.submitInvoice((InvoiceData) this.any);
+			}
+		};
+
+		this.hotel.getProcessor().submitBooking(new Booking(Type.DOUBLE, this.hotel, this.arrival, this.departure, NIF));
+
+		Hotel.cancelBooking("XPTO");
 	}
 
 	@Test
