c668b2c940466e7703f43b8da836c8a31830f69e
Samuel Santos <samuel.c.santos@tecnico.ulisboa.pt>
2018-04-14 19:32:55 +0100

    closes #273

--- car/src/main/java/pt/ulisboa/tecnico/softeng/car/domain/Car.java
+++ car/src/main/java/pt/ulisboa/tecnico/softeng/car/domain/Car.java
@@ -1,6 +1,6 @@
 package pt.ulisboa.tecnico.softeng.car.domain;
 
-public class Car extends Vehicle {
+public class Car extends Car_Base {
 	public Car(String plate, int kilometers, double price, RentACar rentACar) {
 		super(plate, kilometers, price, rentACar);
 	}
