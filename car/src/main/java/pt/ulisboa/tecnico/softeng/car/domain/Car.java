bf2dca204d2d9dcaed4a39b0629ba637b8126927
Joao Almeida <joao.santos.almeida@tecnico.ulisboa.pt>
2018-04-14 21:59:30 +0100

    fix car constructor

--- car/src/main/java/pt/ulisboa/tecnico/softeng/car/domain/Car.java
+++ car/src/main/java/pt/ulisboa/tecnico/softeng/car/domain/Car.java
@@ -2,8 +2,6 @@ package pt.ulisboa.tecnico.softeng.car.domain;
 
 public class Car extends Car_Base {
 
-	public Car(){}
-
 	public Car(String plate, int kilometers, double price, RentACar rentACar){
 		super();
 		init(plate, kilometers, price, rentACar);
