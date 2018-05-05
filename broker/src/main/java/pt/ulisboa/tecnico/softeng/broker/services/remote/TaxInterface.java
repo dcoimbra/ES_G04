bac7b6dea8c93851d3e6a42a4bfe3db65bf17b2f
Joao Almeida <joao.santos.almeida@tecnico.ulisboa.pt>
2018-05-05 17:18:29 +0100

    closes #458

--- broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/services/remote/TaxInterface.java
+++ broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/services/remote/TaxInterface.java
@@ -1,16 +1,38 @@
 package pt.ulisboa.tecnico.softeng.broker.services.remote;
 
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 import pt.ulisboa.tecnico.softeng.broker.services.remote.dataobjects.InvoiceData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TaxInterface {
+	private static Logger logger = LoggerFactory.getLogger(TaxInterface.class);
+
 	public static String submitInvoice(InvoiceData invoiceData) {
-		// return IRS.submitInvoice(invoiceData);
-		// TODO: implement in the final version as a rest invocation
-		return null;
+		RestTemplate restTemplate = new RestTemplate();
+		String url = "http://localhost:8086/taxpayers/submitinvoice";
+		String response;
+		try{
+			response = restTemplate.postForObject(url, invoiceData, String.class);
+		} catch (RestClientException e){
+			logger.error("Failed to get {} due to error: {}", url, e.getMessage());
+			return null;
+		}
+
+		return response;
 	}
 
 	public static void cancelInvoice(String invoiceReference) {
-		// IRS.cancelInvoice(invoiceReference);
-		// TODO: implement in the final version as a rest invocation
+		RestTemplate restTemplate = new RestTemplate();
+		String url = "http://localhost:8086/taxpayers/submitinvoice";
+		try{
+			restTemplate.postForObject(url, invoiceReference, String.class);
+		} catch (RestClientException e){
+			logger.error("Failed to get {} due to error: {}", url, e.getMessage());
+			return;
+		}
+
+		return;
 	}
 }
