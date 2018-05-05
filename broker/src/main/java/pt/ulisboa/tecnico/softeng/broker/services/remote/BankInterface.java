f45e68f3c03c035050755bf5dc263525ba1736cb
Joao Almeida <joao.santos.almeida@tecnico.ulisboa.pt>
2018-05-05 15:14:04 +0100

    closes #453

--- broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/services/remote/BankInterface.java
+++ broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/services/remote/BankInterface.java
@@ -1,23 +1,33 @@
 package pt.ulisboa.tecnico.softeng.broker.services.remote;
 
+import org.springframework.web.client.RestTemplate;
 import pt.ulisboa.tecnico.softeng.broker.services.remote.dataobjects.BankOperationData;
+
+import org.joda.time.DateTime;
 
 public class BankInterface {
 	public static String processPayment(String IBAN, double amount) {
-		// return Bank.processPayment(IBAN, amount);
-		// TODO: implement in the final version as a rest invocation
-		return null;
+		DateTime time = DateTime.now();
+		BankOperationData bankData = new BankOperationData();
+		bankData.setIban(IBAN);
+		bankData.setType("WITHDRAW");
+		bankData.setTime(time);
+		bankData.setValue(amount);
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		return restTemplate.postForObject("http://localhost:8082/banks/processPayment", bankData , String.class);
 	}
 
 	public static String cancelPayment(String reference) {
-		// return Bank.cancelPayment(reference);
-		// TODO: implement in the final version as a rest invocation
-		return null;
+
+		RestTemplate restTemplate = new RestTemplate();
+		return restTemplate.postForObject("http://localhost:8082/banks/cancelPayment", reference , String.class);
 	}
 
 	public static BankOperationData getOperationData(String reference) {
-		// return Bank.getOperationData(reference);
-		// TODO: implement in the final version as a rest invocation
-		return null;
+
+		RestTemplate restTemplate = new RestTemplate();
+		return restTemplate.getForObject("http://localhost:8082/banks/operationData/" + reference, BankOperationData.class);
 	}
 }
