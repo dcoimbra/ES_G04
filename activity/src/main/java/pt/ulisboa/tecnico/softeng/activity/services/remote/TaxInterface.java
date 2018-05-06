d993b3d61dc3b226dcb9f141055e78d0195c9d7d
José Ferrão <jozeus.alves@gmail.com>
2018-05-06 19:24:37 +0100

    closes #478

--- activity/src/main/java/pt/ulisboa/tecnico/softeng/activity/services/remote/TaxInterface.java
+++ activity/src/main/java/pt/ulisboa/tecnico/softeng/activity/services/remote/TaxInterface.java
@@ -1,16 +1,47 @@
 package pt.ulisboa.tecnico.softeng.activity.services.remote;
 
+
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
 import pt.ulisboa.tecnico.softeng.activity.services.remote.dataobjects.InvoiceData;
+import pt.ulisboa.tecnico.softeng.activity.services.remote.exceptions.TaxException;
+import pt.ulisboa.tecnico.softeng.activity.services.remote.exceptions.RemoteAccessException;
+
 
 public class TaxInterface {
+
+	private static String ENDPOINT = "http://localhost:8086";
+
 	public static String submitInvoice(InvoiceData invoiceData) {
-		// return IRS.submitInvoice(invoiceData);
-		// TODO: implement in the final version as a rest invocation
-		return null;
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		try{
+			String response = restTemplate.postForObject(ENDPOINT + "/rest/tax/submitinvoice", invoiceData, String.class);
+
+			return response;
+
+		} catch (HttpClientErrorException e) {
+			throw new TaxException();
+
+		} catch (Exception e) {
+			throw new RemoteAccessException();
+		}
 	}
 
 	public static void cancelInvoice(String invoiceReference) {
-		// IRS.cancelInvoice(invoiceReference);
-		// TODO: implement in the final version as a rest invocation
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		try{
+			restTemplate.postForObject(ENDPOINT + "/rest/tax/cancel?reference=" + invoiceReference,null, String.class);
+
+		} catch (HttpClientErrorException e) {
+			throw new TaxException();
+
+		} catch (Exception e) {
+			throw new RemoteAccessException();
+		}
 	}
 }
